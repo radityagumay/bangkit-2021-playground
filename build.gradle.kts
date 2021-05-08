eb7b2fb1a08ec7c4c82c10c532396e2eca69cbd5
radityagumay <gumay.raditya@gmail.com>
2021-05-08 07:03:57 +0700

    Added HtmlHook Framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,19 @@ group = "me.user"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven { setUrl("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
+    jcenter()
     mavenCentral()
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("stdlib-js"))
+    // include for server side
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.1")
+    // include for client-side
+    implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.7.1")
+
     testImplementation(kotlin("test-junit"))
 }
 
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,103 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlinx.html.*
+import kotlinx.html.stream.appendHTML
+
+fun main() {
+    val uiHook: FrameworkHook = HtmlHook()
+    FeedView(uiHook)
 }
 
+interface FrameworkHook {
+    fun display(feed: Feed)
+}
 
+class HtmlHook : FrameworkHook {
+    override fun display(feed: Feed) {
+        System.out.appendHTML().html {
+            body {
+                h1 {
+                    +"Feed Id: ${feed.id}"
+                }
+
+                p {
+                    +"Feed Description : ${feed.description}"
+                }
+
+                div {
+                    +"Date : ${feed.date}"
+                    p {
+                        +"Url : ${feed.url}"
+                    }
+                }
+            }
+        }
+    }
+}
+
+class FeedView(
+    private val uiHook: FrameworkHook
+) : FeedContract.View {
+
+    private val repository: FeedRepository = FakeFeedRepositoryImpl()
+    private val presenter: FeedContract.Presenter = PresenterImpl(this, repository)
+
+    init {
+        presenter.execute()
+    }
+
+    override fun feedsIsLoaded(feeds: List<Feed>) {
+        feeds.forEach(uiHook::display)
+    }
+}
+
+interface FeedContract {
+    interface Presenter {
+        fun execute()
+    }
+
+    interface View {
+        fun feedsIsLoaded(feeds: List<Feed>)
+    }
+}
+
+class PresenterImpl(
+    private val view: FeedContract.View,
+    private val repository: FeedRepository
+) : FeedContract.Presenter {
+    override fun execute() {
+        view.feedsIsLoaded(repository.feeds())
+    }
+}
+
+interface FeedRepository {
+    fun feeds(): List<Feed>
+}
+
+data class Feed(
+    val id: String,
+    val url: String,
+    val description: String,
+    val date: Long
+)
+
+// Fake
+class FakeFeedRepositoryImpl : FeedRepository {
+    override fun feeds(): List<Feed> {
+        return FeedFactory.create()
+    }
+}
+
+// Stub
+object FeedFactory {
+    fun create(): List<Feed> {
+        return listOf(
+            Feed(
+                id = "12-abc",
+                url = "https://www.google.com",
+                description = """
+                    lorem ipsum dolom sir amet
+                """.trimIndent(),
+                date = System.currentTimeMillis()
+            )
+        )
+    }
+}
